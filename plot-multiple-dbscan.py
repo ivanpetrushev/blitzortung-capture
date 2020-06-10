750ef1c63bd880757d2917d8879bf6fb5e7705f6
Ivan Petrushev <ivanatora@gmail.com>
2020-06-10 16:40:13 +0300

    multiple dbscans: plot centroids

--- plot-multiple-dbscan.py
+++ plot-multiple-dbscan.py
@@ -3,6 +3,8 @@ import pandas as pd
 import numpy as np
 from sklearn.cluster import DBSCAN
 from glob import glob
+from shapely.geometry import MultiPoint
+from geopy.distance import great_circle
 
 DBSCAN_EPS = 20/6371
 DBSCAN_MIN = 5
@@ -11,13 +13,21 @@ filter_geohash = ['u2', 'u8', 'sr', 'sx']  # Bulgaria
 
 out = []
 labels = []
+file_idx = 0
 
 
 def shorten_geohash(row):
     return row['geohash'][:2]
 
 
+def get_centermost_point(cluster):
+    centroid = (MultiPoint(cluster).centroid.x, MultiPoint(cluster).centroid.y)
+    centermost_point = min(cluster, key=lambda point: great_circle(point, centroid).m)
+    return tuple(centermost_point)
+
+
 for file in glob('capture-2020*'):
+    file_idx += 1
     df = pd.read_json(file)
     if df.empty:
         continue
@@ -34,13 +44,25 @@ for file in glob('capture-2020*'):
     clusters = pd.Series([coords[cluster_labels == n] for n in range(num_clusters)])
     print('Number of clusters: {}'.format(num_clusters))
 
-    for i, cluster in enumerate(clusters):
-        for point in cluster:
-            out.append({
-                'lat': point[0],
-                'lon': point[1]
-            })
-            labels.append(i)
+    # add Centroids to output
+    centermost_points = clusters.map(get_centermost_point)
+    for i, point in enumerate(centermost_points):
+        out.append({
+            'lat': point[0],
+            'lon': point[1]
+        })
+        labels.append(file_idx)
+
+    # add cluster members to output
+    # for i, cluster in enumerate(clusters):
+    #     for point in cluster:
+    #         out.append({
+    #             'lat': point[0],
+    #             'lon': point[1]
+    #         })
+    #         labels.append(i)
+
+
 dfout = pd.DataFrame(out)
 dfout.plot.scatter('lon', 'lat', s=2, c=labels, cmap='tab20b')
 # plt.plot(dfout)
