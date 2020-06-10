c5c324e69fca38dfc58fe83339ad8c10d578ffb8
Ivan Petrushev <ivanatora@gmail.com>
2020-06-10 16:32:37 +0300

    plot multiple dbscan captures

--- plot-multiple-dbscan.py
+++ plot-multiple-dbscan.py
@@ -9,18 +9,21 @@ DBSCAN_MIN = 5
 
 filter_geohash = ['u2', 'u8', 'sr', 'sx']  # Bulgaria
 
+out = []
+labels = []
+
 
 def shorten_geohash(row):
     return row['geohash'][:2]
 
 
 for file in glob('capture-2020*'):
-    print('file', file)
     df = pd.read_json(file)
     if df.empty:
         continue
     df['short_geohash'] = df.apply(lambda row: shorten_geohash(row), axis=1)
     df = df[df.short_geohash.isin(filter_geohash)]
+    print(f'file: {file} strikes: {len(df)}')
 
     coords = df.drop(['geohash', 'short_geohash'], axis=1).values
 
@@ -30,8 +33,7 @@ for file in glob('capture-2020*'):
     num_clusters = len(set(cluster_labels)) - 1  # -1 ?
     clusters = pd.Series([coords[cluster_labels == n] for n in range(num_clusters)])
     print('Number of clusters: {}'.format(num_clusters))
-    out = []
-    labels = []
+
     for i, cluster in enumerate(clusters):
         for point in cluster:
             out.append({
@@ -39,6 +41,7 @@ for file in glob('capture-2020*'):
                 'lon': point[1]
             })
             labels.append(i)
-    dfout = pd.DataFrame(out)
-    dfout.plot.scatter('lon', 'lat', s=1, c=labels, cmap='tab20b')
+dfout = pd.DataFrame(out)
+dfout.plot.scatter('lon', 'lat', s=2, c=labels, cmap='tab20b')
+# plt.plot(dfout)
 plt.show()
