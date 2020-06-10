8958ff21503d009ff05738a0445a8ac1105a198c
Ivan Petrushev <ivanatora@gmail.com>
2020-06-10 13:20:36 +0300

    plot with pandas

--- plot.py
+++ plot.py
@@ -5,29 +5,14 @@ import numpy as np
 
 filter_geohash = ['u0', 'u1', 'u2', 'u3', 'u8', 'u9', 'sp', 'sr', 'sx']
 
-with open('capture.json') as fp:
-    strikes = json.load(fp)
 
-lats = []
-lons = []
-filtered_strikes = []
+def shorten_geohash(row):
+    return row['geohash'][:2]
 
-for i, item in enumerate(strikes):
-    in_filter = False
-    for gh_start in filter_geohash:
-        if item['geohash'].startswith(gh_start):
-            in_filter = True
-            break
-    if not in_filter:
-        continue
-    item['lat'] *= 1000000
-    item['lon'] *= 1000000
-    filtered_strikes.append(item)
-    lats.append(item['lat'])
-    lons.append(item['lon'])
 
-# print(lats)
-# print(lons)
-print('Number of filtered strikes', len(filtered_strikes))
-plt.scatter(lons, lats, s=1)
+df = pd.read_json('capture.json')
+df['short_geohash'] = df.apply(lambda row: shorten_geohash(row), axis=1)
+df = df[df.short_geohash.isin(filter_geohash)]
+
+df.plot.scatter('lon', 'lat', s=1)
 plt.show()
